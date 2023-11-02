89116957b6c781535d0ffa0aa973b6f950eb7698
Jpuc1143 <jaime.perez@uc.cl>
2023-11-02 12:53:51 -0300

    Chore: configure eslint and prettier

--- src/grabaciones.js
+++ src/grabaciones.js
@@ -1,24 +1,33 @@
 window.onload = async () => {
-	const r = await fetch(process.env.API_URL + "/recordings");
-	const recordings = await r.json();
+  const r = await fetch(process.env.API_URL + "/recordings");
+  const recordings = await r.json();
 
-	const listElement = document.getElementById("recordingsList");
-	recordings.forEach(recording => {
-		const node = document.createElement("li")
-		node.append("Nombre: " + recording.name + ", Fecha: " + recording.created_at + ", ID: " + recording.id)
+  const listElement = document.getElementById("recordingsList");
+  recordings.forEach((recording) => {
+    const node = document.createElement("li");
+    node.append(
+      "Nombre: " +
+        recording.name +
+        ", Fecha: " +
+        recording.created_at +
+        ", ID: " +
+        recording.id,
+    );
 
-		const downloadButton = document.createElement("a")
-		downloadButton.href = process.env.API_URL + "/recordings/" + recording.id
-		downloadButton.append("Descargar")
+    const downloadButton = document.createElement("a");
+    downloadButton.href = process.env.API_URL + "/recordings/" + recording.id;
+    downloadButton.append("Descargar");
 
-		const deleteButton = document.createElement("button")
-		deleteButton.onclick = async e => {
-			await fetch(process.env.API_URL + "/recordings/" + recording.id, {method: "DELETE"})
-		}
-		deleteButton.append("Borrar")
+    const deleteButton = document.createElement("button");
+    deleteButton.onclick = async () => {
+      await fetch(process.env.API_URL + "/recordings/" + recording.id, {
+        method: "DELETE",
+      });
+    };
+    deleteButton.append("Borrar");
 
-		listElement.append(node)
-		listElement.append(downloadButton)
-		listElement.append(deleteButton)
-	})
-}
+    listElement.append(node);
+    listElement.append(downloadButton);
+    listElement.append(deleteButton);
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,83 +1,86 @@
-import Chart from "chart.js/auto"
+import Chart from "chart.js/auto";
 
-let recordingStatus = false
+let recordingStatus = false;
 
 window.onload = () => {
-  const ctx = document.getElementById('sensorsChart');
-  sensorsChart = new Chart(ctx, {
-    type: 'scatter',
+  const ctx = document.getElementById("sensorsChart");
+  const sensorsChart = new Chart(ctx, {
+    type: "scatter",
     data: {
-      datasets: [{
-        label: '# of Votes',
-        data: [{x: 1, y: 2}],
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [{ x: 1, y: 2 }],
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
-	    showLine: true,
+      showLine: true,
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+  const apiURL = new URL(process.env.API_URL);
+  let wsProtocol;
+  if (apiURL.protocol === "https:") {
+    wsProtocol = "wss://";
+  } else {
+    wsProtocol = "ws://";
+  }
+
+  const socket = new WebSocket(wsProtocol + apiURL.host + "/ws");
+  socket.addEventListener("message", async (event) => {
+    console.log(event.data);
+    const msg = JSON.parse(event.data);
+    switch (msg.type) {
+      case "setup":
+        sensorsChart.data.datasets = msg.sensors.map((setup) => {
+          return { label: setup.id };
+        });
+        sensorsChart.update();
+        break;
+
+      case "data":
+        sensorsChart.data.datasets.forEach((dataset) => {
+          dataset.data.push(msg.data[dataset.label]);
+        });
+        sensorsChart.update();
+        break;
+
+      default:
+        console.log("Unknown msg type", event.data);
     }
   });
-	const apiURL = new URL(process.env.API_URL)
-	let wsProtocol;
-	if (apiURL.protocol === "https:") {
-		wsProtocol = "wss://"
-	} else {
-		wsProtocol = "ws://"
-	}
 
-	const socket = new WebSocket(wsProtocol + apiURL.host + "/ws")
-	socket.addEventListener("message", async event => {
-		console.log(event.data)
-		const msg = JSON.parse(event.data)
-		switch (msg.type) {
-			case "setup":
-				sensorsChart.data.datasets = msg.sensors.map(setup => {
-					return {label: setup.id}
-				})
-				sensorsChart.update()
-				break;
+  const filenameInput = document.getElementById("filenameInput");
+  console.log(filenameInput);
+  document.getElementById("recordingButton").onclick = async (e) => {
+    e.preventDefault();
+    if (recordingStatus) {
+      recordingStatus = false;
+      e.target.textContent = "Grabar";
+      filenameInput.disabled = false;
+      await socket.send(JSON.stringify({ cmd: "stop-recording" }));
+    } else {
+      recordingStatus = true;
+      e.target.textContent = "Parar grabación";
+      filenameInput.disabled = true;
+      console.log(filenameInput);
+      await socket.send(
+        JSON.stringify({ cmd: "start-recording", name: filenameInput.value }),
+      );
+    }
+  };
 
-			case "data":
-				sensorsChart.data.datasets.forEach(dataset => {
-					dataset.data.push(msg.data[dataset.label])
-				})
-				sensorsChart.update()
-				break;
-
-			default:
-				console.log("Unknown msg type", event.data)
-
-		}
-	})
-
-	const filenameInput = document.getElementById("filenameInput")
-	console.log(filenameInput)
-	document.getElementById("recordingButton").onclick = async e => {
-		e.preventDefault()
-		if (recordingStatus) {
-			recordingStatus = false
-			e.target.textContent = "Grabar"
-			filenameInput.disabled = false
-			await socket.send(JSON.stringify({cmd: "stop-recording"}))
-		} else {
-			recordingStatus = true
-			e.target.textContent = "Parar grabación"
-			filenameInput.disabled = true
-			console.log(filenameInput)
-			await socket.send(JSON.stringify({cmd: "start-recording", name: filenameInput.value}))
-		}
-	}
-
-	document.getElementById("clearGraphsButton").onclick = e => {
-		e.preventDefault()
-		sensorsChart.data.datasets.forEach(dataset => {
-			dataset.data.length = 0
-		})
-		sensorsChart.update()
-	}
-}
+  document.getElementById("clearGraphsButton").onclick = (e) => {
+    e.preventDefault();
+    sensorsChart.data.datasets.forEach((dataset) => {
+      dataset.data.length = 0;
+    });
+    sensorsChart.update();
+  };
+};
